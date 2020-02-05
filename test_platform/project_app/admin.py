1afac55363057efc68f023bf533a50dd7033d6ac
D6666666 <1657450321@qq.com>
2020-02-05 16:33:14 +0800

    添加前端样式，补充数据模型，映射admin

--- test_platform/project_app/admin.py
+++ test_platform/project_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from project_app.models import Project,Module
 
 # Register your models here.
+#映射到admin后台管理系统
+class ProjectAdmin(admin):
+    list_display = ['name','status','create_time','id']
+
+class ModuleAdmin(admin):
+    list_display = ['name','create_time','id']
+
+admin.site.register(Project)
+admin.site.register(Module)
--- test_platform/project_app/models.py
+++ test_platform/project_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+    """
+    项目表
+    """
+    name = models.CharField('名称',max_length=100,blank=False,default='')
+    describe = models.TextField('描述',default='')
+    status = models.BooleanField('状态',default=True)
+    create_time = models.DateTimeField('创建时间',auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Module(models.Model):
+    """
+    模块表
+    """
+    project = models.ForeignKey(Project,on_delete=models.CASCADE) #on_delete=models.CASCADE外健联级删除
+    name = models.CharField("名称",max_length=100,blank=False,default='')
+    describe = models.TextField("描述",default='')
+    create_time = models.DateTimeField('创建时间',auto_now=True)
+
+    def __str__(self):
+        return self.name
