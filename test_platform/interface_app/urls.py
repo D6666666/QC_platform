dc68d0d66c4a45a0a44d45c1395dce03d81167c6
D6666666 <1657450321@qq.com>
2020-02-08 23:43:35 +0800

    完成基本调试功能，优化ajax处理页面，异步加载

--- test_platform/interface_app/urls.py
+++ test_platform/interface_app/urls.py
@@ -8,4 +8,5 @@ from interface_app import views
 urlpatterns = [
     path('case_manage/', views.case_manage),
     path('api_debug/', views.api_debug),
+    path('debug/', views.debug),
 ]
--- test_platform/interface_app/views.py
+++ test_platform/interface_app/views.py
@@ -1,6 +1,9 @@
+import requests,json,validators
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from test_platform import common
 # Create your views here.
 
 #请求用例管理页面
@@ -13,8 +16,63 @@ def case_manage(request):
 
 #新增&调试用例页面
 @login_required
-def api_debug(request):
+def debug(request):
     if request.method == 'GET':
-        return render(request,'api_debug.html',{'type':'debug'})
+        return render(request,'debug.html',{'type':'debug'})
     else:
         return HttpResponse("404")
+
+
+#接口调试
+@csrf_exempt
+@login_required
+def api_debug(request):
+    if request.method == 'POST':
+        url = request.POST.get("req_url")
+        method = request.POST.get("req_method")
+        type_ = request.POST.get("req_type")
+        header = request.POST.get("req_header")
+        parameter = request.POST.get("req_parameter")
+        if validators.url(url) != True:
+            return common.response_failed("请检查URL！")
+        if method not in ['post','get']:
+            return common.response_failed("请检查请求方法！")
+        print(header)
+        print(type(header))
+        if header != '':
+            try:
+                header_dict = json.loads(header.replace("'", "\""))
+                if type(header_dict) != dict:
+                    raise TypeError
+            except Exception:
+                return common.response_failed("请检查header格式！")
+        else:
+            header_dict = None
+
+        if parameter != '':
+            try:
+                payload = json.loads(parameter.replace("'", "\""))
+                if type(payload) != dict:
+                    raise TypeError
+            except json.decoder.JSONDecodeError:
+                return common.response_failed("请检查参数的格式！")
+        else:
+            payload = None
+
+        if method == "get":
+            if type_ == "from":
+                r = requests.get(url, headers=header_dict, params=payload)
+            else:
+                return common.response_failed("参数类型错误")
+
+        if method == "post":
+            if type_ == "from":
+                r = requests.post(url, headers=header_dict, data=payload)
+            elif type_ == "json":
+                r = requests.post(url, headers=header_dict, json=payload)
+            else:
+                return common.response_failed("参数类型错误")
+        print(r.text)
+        return common.response_succeed(data=r.text)
+    else:
+        return common.response_failed("请求方法错误")
